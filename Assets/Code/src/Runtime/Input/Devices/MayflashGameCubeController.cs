cef9404efd3bd3f0562e5592c28c5b2bc1b3a48b
James Liu <contact@jamessliu.com>
2018-08-01 04:21:51 -0700

    Properly organize bundles

---
+++ Assets/Code/src/Runtime/Input/Devices/MayflashGameCubeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Input;
+using UnityEngine.Experimental.Input.Utilities;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine.Experimental.Input.Editor;
+#endif
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+#endif
+public static class InputExtensions {
+
+  static InputExtensions() {
+    InputSystem.RegisterControlLayout(@"
+{
+    ""name"" : ""MayflashGamepad"",
+    ""extend"" : ""Gamepad"",
+    ""format"" : ""HID"",
+    ""device"" : { 
+      ""interface"" : ""HID"", 
+      ""manufacturer"" : ""mayflash.+"" ,
+      ""product"" : ""MAYFLASH.+"",
+      ""version"" : "".+"" 
+    },
+    ""controls"" : [
+    ]
+}
+    ");
+  }
+
+}
+
+}
